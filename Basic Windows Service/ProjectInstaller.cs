8ace3a717575ca6e23548a6671b0e455e88a0550
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-04-15 23:30:50 +0300

    project to Install Service install service and test it

---
+++ Basic Windows Service/ProjectInstaller.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace Basic_Windows_Service
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller :Installer
+    {
+
+        private ServiceProcessInstaller _serviceProcessInstaller;
+        private ServiceInstaller _serviceInstaller;
+
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+
+            // Configure the Service Process Installer
+            _serviceProcessInstaller = new ServiceProcessInstaller
+            {
+                Account = ServiceAccount.LocalSystem // Adjust as needed (e.g., NetworkService, LocalService)
+            };
+
+            // Configure the Service Installer
+            _serviceInstaller = new ServiceInstaller
+            {
+                ServiceName = "MyFirstWinService" , // Must match the ServiceName in your ServiceBase class
+                DisplayName = "My First Windows Service" ,
+                Description = "MyFirstWinService",
+                StartType = ServiceStartMode.Manual // Or Automatic, depending on requirements
+            };
+
+            // Add installers to the installer collection
+            Installers.Add(_serviceProcessInstaller);
+            Installers.Add(_serviceInstaller);
+        }
+    }
+}
